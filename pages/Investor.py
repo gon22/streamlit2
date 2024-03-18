37630170f03170bd49369a0b2c22726fee1afb48
gon22 <54576555+gon22@users.noreply.github.com>
2024-03-18 15:20:12 +0900

    Update Investor.py

--- pages/Investor.py
+++ pages/Investor.py
@@ -541,11 +541,5 @@ with tab5:
             with col2:
                 # vl = pd.DataFrame(data=[-0.0005,-0.0288,0.0003,-0.0003,0,-0.0007,-0.01,-0.008,0.0015,0.001,-0.0033,0,-0.001,0.0001,0.4692,-0.0143,])
                 predict_data_preprocessed = model[:4].transform(predict_data)
-                st.write(shap.TreeExplainer(model[-1], feature_perturbation="tree_path_dependent").shap_values(predict_data_preprocessed))
-                # try:
-                #     shap_value = shap.TreeExplainer(model[-1], feature_perturbation="tree_path_dependent").shap_values(predict_data_preprocessed)[1]
-                #     st_shap(shap.waterfall_plot(shap.Explanation(values=shap_value[0], base_values=0.5, data=predict_data_preprocessed.values[0], feature_names=predict_data_preprocessed.columns.tolist())), height=500, width=900)
-                # except:
-                #     vl = pd.DataFrame(data=[-0.0005,-0.0288,0.0003,-0.0003,0,-0.0007,-0.01,-0.008,0.0015,0.001,-0.0033,0,-0.001,0.0001,0.4692,-0.0143,])
-                #     predict_data_preprocessed = model[:4].transform(predict_data)
-                #     st_shap(shap.waterfall_plot(shap.Explanation(values=vl[0], base_values=0.5, data=predict_data_preprocessed.values[0], feature_names=predict_data_preprocessed.columns.tolist())), height=500, width=900)
+                shap_value = np.array(shap.TreeExplainer(model[-1], feature_perturbation="tree_path_dependent").shap_values(predict_data_preprocessed)[0])[:, 1]
+                st_shap(shap.waterfall_plot(shap.Explanation(values=shap_value[0], base_values=0.5, data=predict_data_preprocessed.values[0], feature_names=predict_data_preprocessed.columns.tolist())), height=500, width=900)
